97dae003511278c08cf59f93a69bdfbb275ab91b
maeorg <katrina.maeorg@gmail.com>
2023-09-06 09:55:00 +0300

    a bit more restructuring in regards of adding frontend

--- frontend/videorental-frontend-angular/src/app/app.component.spec.ts
+++ frontend/videorental-frontend-angular/src/app/app.component.spec.ts
@@ -1,10 +1,8 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
     declarations: [AppComponent]
   }));
 
@@ -14,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'videorental-frontend'`, () => {
+  it(`should have as title 'videorental-frontend-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('videorental-frontend');
+    expect(app.title).toEqual('videorental-frontend-angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('videorental-frontend app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('videorental-frontend-angular app is running!');
   });
 });
